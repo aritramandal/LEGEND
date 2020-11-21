d30f4d9a88b43b912eb51404bb013eadedea7a71
Roy <71298965+suhaash02@users.noreply.github.com>
2020-11-21 08:55:42 +0530

    Update survivorpfp.py

--- survivorpfp.py
+++ survivorpfp.py
@@ -1,6 +1,6 @@
 """
 Time In Profile Pic.....
-Command: `.survivorpfp`
+Command: `.surpfp`
 
 Custom / Modified Plugin for some magical effects by this Legendary Guy @Sur_vivor
 
@@ -33,14 +33,16 @@ TELEGRAPH_MEDIA_LINKS = ["https://telegra.ph/file/2eab4f64ead6fbf41bf87.jpg",
                          "https://telegra.ph/file/1270ed675db61e6c84eea.jpg",
                          "https://telegra.ph/file/32743c9389915b02fdea7.jpg",
                          "https://telegra.ph/file/8c02a1430502bea931ff7.jpg",
-                         "https://telegra.ph/file/1ec37d367bb59ac56131d.jpg",
-                         "https://telegra.ph/file/e9aeef4fd2e3d0b9e9f24.jpg",
                          "https://telegra.ph/file/28c242ea9f8cf32db4c21.jpg",
                          "https://telegra.ph/file/c089426ca031d1f6297b0.jpg",
                          "https://telegra.ph/file/a196b6c07f0a659daf058.jpg",
-                         "https://telegra.ph/file/69f19acd13b1eaf3fc120.jpg"
+                         "https://telegra.ph/file/69f19acd13b1eaf3fc120.jpg",
+                         "https://telegra.ph/file/9478a8a10a5457201b9bd.jpg",
+                         "https://telegra.ph/file/2765633721f6e21a0df9e.jpg",
+                         "https://telegra.ph/file/b371ebcd42849d093bfc2.jpg",
+                         "https://telegra.ph/file/19b004330bf89c13f0d1d.jpg",
                         ]
-@borg.on(admin_cmd(pattern="survivorpfp ?(.*)"))
+@borg.on(admin_cmd(pattern="surpfp ?(.*)"))
 async def autopic(event):
     while True:
         piclink = random.randint(0, len(TELEGRAPH_MEDIA_LINKS) - 1)
@@ -55,7 +57,7 @@ async def autopic(event):
     
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
-        current_time = datetime.now().strftime("@Sur_vivor \n \nTime: %H:%M:%S \nDate: %d/%m/%y")
+        current_time = datetime.now().strftime("@peru_monster \n \nTime: %H:%M:%S \nDate: %d/%m/%y")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 23)
