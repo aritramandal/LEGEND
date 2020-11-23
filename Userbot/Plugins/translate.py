b05e383e89560b6743f16df21def5074e137841d
@YOU_ARE_UNDER_ARREST <72908226+aritramandal@users.noreply.github.com>
2020-11-23 18:13:06 +0530

    Update translate.py

--- Userbot/Plugins/translate.py
+++ Userbot/Plugins/translate.py
@@ -5,7 +5,7 @@ Available Commands:
 
 import emoji
 from googletrans import Translator
-from userbot.utils import admin_cmd
+from Userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd(pattern="tl ?(.*)"))
