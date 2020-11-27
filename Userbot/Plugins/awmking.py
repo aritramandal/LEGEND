4f2af1e6b6984c45e71336de4c92c080a48892b7
@YOU_ARE_UNDER_ARREST <72908226+aritramandal@users.noreply.github.com>
2020-11-27 07:31:26 +0530

    Update awmking.py

--- Userbot/Plugins/awmking.py
+++ Userbot/Plugins/awmking.py
@@ -3,8 +3,8 @@ import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from platform import uname
-from userbot import CMD_HELP
-from userbot.utils import admin_cmd
+from Userbot import CMD_HELP
+from Userbot.utils import admin_cmd
 B = ("▄︻╦芫≡══-------------➖")
 @borg.on(admin_cmd(pattern=r"awm"))
 async def bluedevilawm(awm):
