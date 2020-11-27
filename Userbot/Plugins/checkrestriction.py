2e69ebefa3174128f17f41ec5955c83d4e8b3b4b
@YOU_ARE_UNDER_ARREST <72908226+aritramandal@users.noreply.github.com>
2020-11-27 07:32:33 +0530

    Update checkrestriction.py

--- Userbot/Plugins/checkrestriction.py
+++ Userbot/Plugins/checkrestriction.py
@@ -3,7 +3,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.utils import admin_cmd
+from Userbot.utils import admin_cmd
 
 @borg.on(admin_cmd(pattern="cres ?(.*)"))
 async def _(event):
